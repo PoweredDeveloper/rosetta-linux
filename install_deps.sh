d75ab938bf2851808c2c32d42faa1b7d4d1e60eb
Mikis <115712692+PoweredDeveloper@users.noreply.github.com>
2025-11-05 11:44:26 -0500

    Commit

---
+++ install_deps.sh
@@ -0,0 +1,5 @@
+sudo dpkg --add-architecture amd64
+sudo dpkg --add-architecture i386
+sudo apt update
+
+sudo apt install libbz2-1.0:amd64 libglib2.0-0:amd64 libnsl-dev:amd64 libfontconfig:amd64 libx11-xcb1:amd64 libxext6:amd64 libsm6:amd64 libdbus-1-3:amd64 libxft2:amd64 libxtst6:amd64 libxi6:amd64 libgtk2.0-0:amd64 libcrypt1:amd64 libgtk-3-0:amd64
